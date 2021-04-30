4e5c2057d982fd86cb8e0c6cbb6094f76faefcca
Sylvain Janet <sjanet@dawan.fr>
2021-04-30 17:49:30 +0200

    WIP savingSystem - encryption integration : first steps

--- src/ts/savingSystem/versions/0.0/0.0.2/SavingSystem0_0_2.ts
+++ src/ts/savingSystem/versions/0.0/0.0.2/SavingSystem0_0_2.ts
@@ -4,10 +4,27 @@ import type { ISavingSystem0_0 } from "./../_interfaces/ISavingSystem0_0";
 import { SavingConstants0_0_2 } from "./SavingConstants0_0_2";
 import { SavingVersion } from "../../../SavingVersion";
 import { SaveConverter0_0_2 } from "./SaveConverter0_0_2";
+import type { ISaveDataInstance0_0 } from "../_interfaces/ISaveData0_0";
+import { LogService } from "../../../../log/logService";
 
 export const SavingSystem0_0_2: ISavingSystem0_0 = getSavingSystem0_0(
   SaveDataStatic0_0_2,
   SavingConstants0_0_2,
   SavingVersion["0.0.2"],
-  SaveConverter0_0_2
+  SaveConverter0_0_2,
+  (save: ISaveDataInstance0_0): string => {
+    LogService.addLevel("SavingSystem0_0_0.encryptAndFormatSave");
+    const result = JSON.stringify(save);
+    LogService.removeLevelResultPrimitive(result);
+    return result;
+  },
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (file: string): any => {
+    LogService.addLevel("SavingSystem0_0_0.unformatAndDecryptSave");
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const result = JSON.parse(file);
+    LogService.removeLevelResultObject(result);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return result;
+  }
 );
